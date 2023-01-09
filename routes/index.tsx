5d594763f0f8ac922d39a422bbc8aa69206895f4
Kaisei <me@kaisei.dev>
2023-01-09 17:34:23 +0900

    fix

--- routes/index.tsx
+++ routes/index.tsx
@@ -27,15 +27,15 @@ return (
             </tr>
             <tr>
                <td>GitHub</td> 
-               <td>me@kaisei.dev</td>
-            </tr>
-            <tr>
-               <td>Blog</td> 
-               <td><a href="https://zenn.dev/k41531">zenn.dev/k41531</a></td>
+               <td><a href="https://github.com/k41531">github.com/k41531</a></td>
             </tr>
             <tr>
                <td>Twitter</td> 
                <td><a href="https://twitter.com/k41531">twitter.com/k41531</a></td>
+            </tr>
+            <tr>
+               <td>Blog</td> 
+               <td><a href="https://zenn.dev/k41531">zenn.dev/k41531</a></td>
             </tr>
           </tbody>
         </table>
