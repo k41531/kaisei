a7d0dd065d99da5f0d1c170cc394704297d1e544
Kaisei <me@kaisei.dev>
2023-01-09 17:32:46 +0900

    add: profile

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,49 @@
 import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
-    </>
-  );
+return (
+  <>
+    <Head>
+      <title>Kaisei</title>
+    </Head>
+    <header>
+      ~
+      <hr/>
+    </header>
+    <main>
+      <section>
+        <h2>
+          About me
+        </h2>
+        <table>
+          <tbody>
+            <tr>
+               <td>Name</td> 
+               <td>Kaisei</td>
+            </tr>
+            <tr>
+               <td>Email</td> 
+               <td>me@kaisei.dev</td>
+            </tr>
+            <tr>
+               <td>GitHub</td> 
+               <td>me@kaisei.dev</td>
+            </tr>
+            <tr>
+               <td>Blog</td> 
+               <td><a href="https://zenn.dev/k41531">zenn.dev/k41531</a></td>
+            </tr>
+            <tr>
+               <td>Twitter</td> 
+               <td><a href="https://twitter.com/k41531">twitter.com/k41531</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <section>
+        
+      </section>
+    </main>
+  </>
+);
 }
