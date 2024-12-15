38288d81b0da6ade7ac7a6023fa5615f3efcdd78
Kaisei <me@kaisei.dev>
2024-12-15 20:13:39 +0900

    remove: Head component

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,5 @@
 // layout.tsx
 import type { ComponentChildren } from "preact";
-import { Head } from "fresh/runtime";
 import Header from "./organisms/header.tsx";
 
 interface LayoutProps {
@@ -12,11 +11,11 @@ interface LayoutProps {
 export default function Layout({ children, title, description }: LayoutProps) {
   return (
     <body class="py-4 px-6 grid gap-2 max-w-xl w-full mx-auto bg-primary text-white sm:min-w-64">
-      <Head>
+      <head>
         <title>{`Kaisei's ${title}`}</title>
         <meta name="description" content={description} />
         <link rel="stylesheet" href="/styles.css" />
-      </Head>
+      </head>
       <Header />
       <main class="grid gap-4 w-full">{children}</main>
     </body>
