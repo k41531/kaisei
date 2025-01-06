070bb4956910909d96e907fa87809d521bdec88e
Kaisei <me@kaisei.dev>
2025-01-06 18:22:07 +0900

    fix: update stylesheet link to use asset function and enhance markdown styling

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 // layout.tsx
+import { asset } from "fresh/runtime";
 import Header from "../components/organisms/header.tsx";
 import { define } from "../utils/state.ts";
 
@@ -9,7 +10,7 @@ export default define.page(function App({ Component, state}) {
         <title>{`Kaisei's ${state.title}`}</title>
         <meta name="description" content={state.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href={asset("/styles.css")} />
       </head>
       <body class="py-4 px-6 grid gap-2 max-w-xl w-full mx-auto bg-primary text-white sm:min-w-64">
         <Header />
