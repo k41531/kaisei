44d454ff27cbe3d93e0eb98d75a1f166834b489a
Kaisei <me@kaisei.dev>
2025-01-07 12:22:50 +0900

    feat: add Source Serif 4 font and enhance markdown styling in CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,6 @@ export default {
 
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["BerkeleyMono", "monospace"],
-      },
       backgroundImage: {
         "gradient-radial":
           "linear-gradient(131deg, rgba(255, 255, 255, 0.10) 0%, rgba(228, 228, 228, 0.00) 100%)",
@@ -21,14 +18,5 @@ export default {
         primary: "#252525",
       },
     },
-  },
-  preflight: {
-    // import local fonts
-    "@font-face": [
-      {
-        fontFamily: "BerkeleyMono",
-        src: 'url(/fonts/BerkeleyMono-Regular.woff2) format("woff2")',
-      },
-    ],
   },
 } satisfies Config;
